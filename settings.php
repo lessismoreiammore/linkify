358f64c780e6ac5ef7aa67b29c69a2335afb322d
Katarina C <katarina.chernyavskaya@gmail.com>
2017-01-09 19:28:33 +0100

    Add php script and main structure of the page

--- settings.php
+++ settings.php
@@ -2,3 +2,41 @@
 session_start();
 require("resources/lib/functions.php");
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="/resources/css/style.css">
+		<title></title>
+	</head>
+	<body>
+
+		<?php
+
+		require("resources/blocks/components/header.php");
+
+		$uid = $_SESSION["login"]["uid"];
+		$users = dbGet($connection, "SELECT * FROM users WHERE id = '$uid';");
+
+		foreach($users as $user) {
+			$name = $user["name"];
+			$username = $user["username"];
+			$email = $user["email"];
+			$avatar = $user["avatar"];
+		}
+
+        require("resources/blocks/components/error.php");
+        require("resources/blocks/components/message.php");
+		?>
+
+		Name: <?= $name; ?> <br>
+		Username: <?= $username; ?> <br>
+		Email: <?= $email; ?> <br>
+		<a href="/resources/blocks/settings/changeInfo.php">Edit info</a> <br>
+		<a href="/resources/blocks/settings/changePassword.php">Change Password</a> <br>
+		<div class="placeholderAvatar"><img src="/resources/img/users/<?php echo $uid ?>/<?php echo $avatar; ?>" style="width: 100%; height: 100%;" alt=""></div> <br>
+		<a href="/resources/blocks/settings/changeAvatar.php">Change avatar</a> <br>
+
+	</body>
+</html>
