992a075634988bb00e45cb51481a2189c5b17a11
Katarina C <katarina.chernyavskaya@gmail.com>
2017-01-09 20:36:30 +0100

    Add form to make password changes

--- resources/blocks/settings/changePassword.php
+++ resources/blocks/settings/changePassword.php
@@ -6,3 +6,11 @@ require("../../blocks/components/error.php");
 require("../../blocks/components/message.php");
 
 ?>
+
+<form action="../../lib/settings.php" method="POST">
+	<input type="hidden" name="action" value="changePassword">
+	<input type="password" name="newPassword" placeholder="New password">
+	<input type="password" name="repeatPassword" placeholder="Repeat password">
+	<input type="password" name="password" placeholder="Old password">
+	<button type="submit" name="button">Save</button>
+</form>
