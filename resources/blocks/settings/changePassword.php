d94243397875903e10d6dc62900fe5a47a5de33a
Katarina C <katarina.chernyavskaya@gmail.com>
2017-01-09 20:35:49 +0100

    Initial commit to changePassword.php

---
+++ resources/blocks/settings/changePassword.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+require("../../lib/functions.php");
+require("../../blocks/components/header.php");
+require("../../blocks/components/error.php");
+require("../../blocks/components/message.php");
+
+?>
