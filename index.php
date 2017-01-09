55857805cc7096319c8e548bb63d1b6cce3e4768
Katarina C <katarina.chernyavskaya@gmail.com>
2017-01-09 19:23:12 +0100

    Add main page structure and php script to index.php

--- index.php
+++ index.php
@@ -5,3 +5,31 @@ require("resources/lib/functions.php");
 $loggedIn = checkLogin($connection);
 
  ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>Linkify</title>
+     <link rel="stylesheet" href="/resources/css/reset.css">
+ 	 <link rel="stylesheet" href="/resources/css/style.css">
+   </head>
+   <body>
+
+      <?php
+      	  require("resources/blocks/components/header.php");
+
+      	  if ($loggedIn) {
+                  require("resources/blocks/home.php");
+              } else {
+      				require("resources/lib/allPosts.php");
+                  require("resources/blocks/auth.php");
+              }
+
+
+      	 require("resources/blocks/components/error.php");
+           require("resources/blocks/components/message.php");
+      	   ?>
+
+         </body>
+</html>
