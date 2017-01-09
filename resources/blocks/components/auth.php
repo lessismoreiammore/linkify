d36fd576140847ac56fa4de414914e219af8f66a
Katarina C <katarina.chernyavskaya@gmail.com>
2017-01-09 19:41:07 +0100

    Add log in and register forms

---
+++ resources/blocks/components/auth.php
@@ -0,0 +1,15 @@
+<!-- Log in form -->
+<form action="/resources/lib/login.php" method="POST">
+	<input type="text" name="username" placeholder="Email or username">
+	<input type="password" name="password" placeholder="Password">
+	<button type="submit" class="login">Log in</button>
+</form>
+
+<!-- Register form -->
+<form action="/resources/lib/register.php" method="POST">
+	<input type="text" name="fullname" placeholder="Full Name">
+   <input type="text" name="username" placeholder="Username">
+   <input type="email" name="email" placeholder="Email">
+   <input type="password" name="password" placeholder="Password">
+   <button type="submit" class="register">Register for Linkify</button>
+</form>
