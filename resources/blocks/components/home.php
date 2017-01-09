5ec104faabc49927e8e26a18fcedd11a025ec3ab
Katarina C <katarina.chernyavskaya@gmail.com>
2017-01-09 20:25:46 +0100

    Add a general structure for home.php

---
+++ resources/blocks/components/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		Welcome
+		<br>
+		<a href="/settings.php">Profile</a>
+		<br>
+		<a href="/logout.php">Log out</a>
+		<br>
+		<a href="/resources/blocks/components/writePost.php">New Post</a>
+		<br>
+		<a href="/resources/lib/myPosts.php">My own posts</a>
+		<br>
+		<?php require("resources/lib/allPosts.php"); ?>
+	</body>
+</html>
