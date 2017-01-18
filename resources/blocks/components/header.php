b404e13bd63773e1986697392278f2fb301f26a7
Katarina C <katarina.chernyavskaya@gmail.com>
2017-01-18 10:33:59 +0100

    Fix a bug on index with like and dislike counters

--- resources/blocks/components/header.php
+++ resources/blocks/components/header.php
@@ -13,37 +13,31 @@ $message = $_SESSION["message"] ?? "";
 
 <header>
      <div class="container">
-         <div class="row vertical-align">
-			 <div class="col-md-2 col-xs-4">
-				 <div class="logo">
+         <div class="row vertical-align logo">
+			 <div class="col-md-2 col-1">
 		 			<h1>LINKIFY</h1>
-		 		</div>
 			 </div>
 
              <!--If the user is logged in the menu will be shown in the right corner  -->
              <?php if ($loggedIn): ?>
-                 <div class="col-md-4 col-xs-8">
-    				 <div class="welcomeMessage">
+                 <div class="col-md-4 col-2  welcomeMessage">
      					<h4>Welcome, <?php echo $user["username"] ?>!</h4>
-     				</div>
     			 </div>
-    			 <div class="col-md-6 col-xs-12">
+    			 <div class="col-md-6 col-3">
 
-                     <div class="row vertical-align">
+                     <div class="row vertical-align userAvatar">
                          <div class="col-md-2 col-xs-4">
-                             <div class="userAvatar">
                                  <!-- The pick of the avatar if the user is just registred -->
                                  <?php
                                  if(isset($user["avatar"])){?>
-                                     <img class="img-responsive img-circle" src="<?php echo $resourcesDir?>img/users/<?php echo $user["id"]?>/<?php echo $user["avatar"] ?>" alt="user avatar" style="background-size: cover; height: 40px; border:1px solid #333">
+                                     <img class="img-responsive img-circle avatar" src="<?php echo $resourcesDir?>img/users/<?php echo $user["id"]?>/<?php echo $user["avatar"] ?>" alt="user avatar">
                                 <?php
                             }else{?>
-                                <img class="img-responsive img-circle" src="<?php echo $resourcesDir?>img/users/newUser.png" alt="user avatar" style="background-size: cover; height: 40px; border:1px solid #333">
+                                <img class="img-responsive img-circle avatar" src="<?php echo $resourcesDir?>img/users/newUser.png" alt="user avatar">
                             <?php
                             }
                                   ?>
 
-                             </div>
                          </div>
                         <div class="col-md-4 col-xs-8">
                             <button class="btn btn-danger btn-accent white" data-toggle="modal" data-target=".bs-example-modal-lg"  role="button">Write a post</button>
